d5045d2099edcf3053c8de47a6161b971add9ef4
tom wang <hualumi1119@gmail.com>
2020-07-19 13:25:39 +1000

    Initial set up

---
+++ airport_display/airport_display/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  airport_display
+//
+//  Created by syle on 19/7/20.
+//  Copyright © 2020 airport_display. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
