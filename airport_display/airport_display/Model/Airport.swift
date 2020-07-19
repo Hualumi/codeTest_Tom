3377e933aeacc73513e883912af87ab990cd8856
tom wang <hualumi1119@gmail.com>
2020-07-19 13:47:29 +1000

    Set up mvvm structure & inital set up for airport list

---
+++ airport_display/airport_display/Model/Airport.swift
@@ -0,0 +1,16 @@
+//
+//  Airport.swift
+//  airport_display
+//
+//  Created by syle on 19/7/20.
+//  Copyright © 2020 airport_display. All rights reserved.
+//
+
+import Foundation
+
+struct Airport {
+    let name: String
+    let code: String
+    let city: String
+    let state: String
+}
---
+++ airport_display/airport_display/ViewController/AirPortListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AirPortListViewController.swift
+//  airport_display
+//
+//  Created by syle on 19/7/20.
+//  Copyright © 2020 airport_display. All rights reserved.
+//
+
+import UIKit
+
+class AirPortListViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .blue
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
